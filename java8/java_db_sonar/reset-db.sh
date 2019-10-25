c4a9851aada27df84d11ec9eeb6f2df3ab0a30c0
Damon Berry <thedude@kakfa.com>
2019-10-25 00:20:35 -0400

    DAB-17: split test db out to it's own docker

--- java8/java_db_sonar/reset-db.sh
+++ java8/java_db_sonar/reset-db.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
 #
-# Stops the test database (docker container), and restarts it.
+# Stops the postgres database (docker container),
+# deletes it, erases the storage so it comes up blank,
+# starts it up and then runs the setup script on it.
 #
-# Since sonar and test db are in the same docker-compose.yaml,
-# the sonar server will get restarted too.
+# If you see complaints about things already existing,
+# then the database container failed to stop, or the 
+# volumes didn't get deleted.
 #
-# Whereas the sonar server has persistent storage, and will
-# remember its previous state when it boots back up,
-# test db does not, and will wake up clean and fresh.
+# To force it to go down, run these:
 #
-# As a result, we will also rerun the setup-db.sh script.
+#    docker stop postgres_test_db_1
+#    docker rm -v postgres_test_db_1
+#
+# and then run this script again.
+#
+# If it keeps happening (docker can be like that)
+# increase the 2 sleeps below, and try again.
+#
 
+# stop the database container
+./stop-db.sh
 
-./gradlew composeDown composeUp
+# docker needs a couple of seconds to take down the postgres network
+sleep 5s
 
-sleep 1s
+# remove the persistent storage, so the database comes up blank
+docker volume rm -f postgres_java_db_sonar
+docker volume rm -f postgres_java_db_sonar_data
+
+# start it up again
+./start-db.sh
+
+# give it time to start, and then run the setup script
+sleep 5s
 ./setup-db.sh
 
+# give it time to finish
+sleep 2s
