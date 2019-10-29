b8ad79a68c38cbd38859f666d5b375b1ea3dc845
Damon Berry <54727087+the-kakfa-dude@users.noreply.github.com>
2019-10-29 18:35:27 -0400

    DAB-15: simple example project for Node12 and Typescript3 (#38)

---
+++ node12/ts_example/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+npm install
+
+npm outdated
+
+npm run compile
+
+./check.sh
+
+npm test
+
---
+++ node12/ts_example/docker-run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker run -t --rm ts_example:latest ./run.sh
+
